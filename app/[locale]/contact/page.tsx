51164911704c59089ced404d620ab0a12581efc2
Muhammad Adnan <vigodesign826@gmail.com>
2026-05-13 19:55:09 +0500

    contact page updated

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -161,10 +161,10 @@ export default function Contact() {
           </p>
 
           <div className="flex items-start gap-3 mb-5">
-            <div className="w-8 h-8 rounded-full bg-gray5 flex items-center justify-center flex-shrink-0">
-              <svg className="w-4 h-4 fill-white" viewBox="0 0 24 24">
-                <path d="M6.6 10.8c1.4 2.8 3.8 5.1 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1-9.4 0-17-7.6-17-17 0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1L6.6 10.8z" />
-              </svg>
+            <div className="w-8 h-8 rounded-full bg-gray5 flex items-center justify-center flex-shrink-0 fill-white">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="" class="bi bi-whatsapp" viewBox="0 0 16 16">
+  <path d="M13.601 2.326A7.85 7.85 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.9 7.9 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.9 7.9 0 0 0 13.6 2.326zM7.994 14.521a6.6 6.6 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.56 6.56 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592m3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.73.73 0 0 0-.529.247c-.182.198-.691.677-.691 1.654s.71 1.916.81 2.049c.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232"/>
+</svg>
             </div>
             <div className="text-white text-sm font-medium">
               <p className="pb-1">{copy.pakistan}</p>
@@ -186,12 +186,12 @@ export default function Contact() {
               </svg>
             </div>
             <div className="text-white text-sm font-medium">
-              <p className="pb-1">{copy.uk}</p>
-              <a href="tel:+447403195442">+44 740 319-5442</a>
+              <p className="pb-1">{copy.pakistan}</p>
+              <a href="tel:+923005467345">+923005467345</a>
             </div>
           </div>
 
-          <div className="flex items-start gap-3 mb-5">
+          {/* <div className="flex items-start gap-3 mb-5">
             <div className="w-8 h-8 rounded-full bg-gray5 flex items-center justify-center flex-shrink-0">
               <svg className="w-4 h-4 fill-white" viewBox="0 0 24 24">
                 <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
@@ -201,7 +201,7 @@ export default function Contact() {
               <p className="pb-1">{copy.location}</p>
               <Link href="/">{copy.address}</Link>
             </div>
-          </div>
+          </div> */}
           <div className="flex items-start gap-3">
             <div className="w-8 h-8 rounded-full bg-gray5 flex items-center justify-center flex-shrink-0">
               <svg className="w-4 h-4 fill-white" viewBox="0 0 24 24">
